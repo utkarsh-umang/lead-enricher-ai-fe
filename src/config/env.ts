673d25776509cda61ef4efb99eecf2c8245a59f8
utkarsh-umang <utkarsh9891@hotmail.com>
2025-04-05 01:07:39 +0530

    fix: prod to domain URL

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://lead-enricher-ai-be-52034488741.us-central1.run.app";
